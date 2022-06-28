3d558fd55ac4d99bb408304a131cc87deb2e43a2
Jose Ibarra <jose.ibarra@gusto.com>
2022-06-28 15:44:40 -0600

    Link the git message and git ignore global file instead of copying

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -6,10 +6,6 @@ mkdir ~/.dotfiles/backups/"${now}"
 
 mv ~/.gitconfig ~/.dotfiles/backups/"${now}"
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-mv ~/.gitmessage.txt ~/.dotfiles/backups/"${now}"
-ln -s ~/.dotfiles/git/.gitmessage.txt ~/.gitmessage.txt
-mv ~/.gitignore_global ~/.dotfiles/backups/"${now}"
-ln -s ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 mv ~/.ssh/config ~/.dotfiles/backups/"${now}"
 ln -s ~/.dotfiles/ssh/config ~/.ssh/config
 mv ~/.zshrc ~/.dotfiles/backups/"${now}"
