066e3d6b8e705a1bf2c6dce13dddce2b2075357d
Jose Ibarra <jose.ibarra@gusto.com>
2022-06-29 12:27:21 -0600

    Add more defaults to macOS

--- scripts/macos_defaults.sh
+++ scripts/macos_defaults.sh
@@ -2,6 +2,14 @@
 
 # Display all files in Finder
 defaults write com.apple.Finder AppleShowAllFiles TRUE
+# Show all file extensions in Finder
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
 killall Finder
 
@@ -12,6 +20,10 @@ defaults write com.apple.dock autohide-time-modifier -int 0
 # defaults write com.apple.dock autohide-time-modifier -float 0.15
 # Remove animation during show
 defaults write com.apple.dock autohide-delay -float 0
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
 
 killall Dock
 
@@ -23,4 +35,31 @@ defaults write -g KeyRepeat -int 1
 
 # Trackpad tracking speed
 defaults write -g com.apple.mouse.scaling -float 2.0
+
+# Disable auto spelling correction
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Disable auto text completion
+defaults write NSGlobalDomain NSAutomaticTextCompletionEnabled -bool false
+# Disable auto capitalization while typing
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+# Disable auto periods while typing
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+# Disable smart dashes while typing
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+# Disable smart quotes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Always show scroll bars
+defaults write -g AppleShowScrollBars -string "Always"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+echo "MacOS settings changed. Please restart to ensure settings take effect."
 
