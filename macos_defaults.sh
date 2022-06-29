06139e2f1b5b7ff7a28b5d2566ed9ebd31b55641
Jose Ibarra <jose.ibarra@gusto.com>
2022-06-29 10:30:40 -0600

    Add defaults for Dock

--- macos_defaults.sh
+++ macos_defaults.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+# Display all files in Finder
 defaults write com.apple.Finder AppleShowAllFiles TRUE
+
 killall Finder
+
+# Dock show/hide time
+# No animation
+defaults write com.apple.dock autohide-time-modifier -int 0
+# Slight animation
+# defaults write com.apple.dock autohide-time-modifier -float 0.15
+
+killall Dock
 
