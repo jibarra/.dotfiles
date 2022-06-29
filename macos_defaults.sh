4728bc21513eb65c0b90687d201489117943ca52
Jose Ibarra <jose.ibarra@gusto.com>
2022-06-29 10:47:53 -0600

    Add mac defaults for mouse, keyboard, and removing all Dock animations

--- macos_defaults.sh
+++ macos_defaults.sh
@@ -6,10 +6,21 @@ defaults write com.apple.Finder AppleShowAllFiles TRUE
 killall Finder
 
 # Dock show/hide time
-# No animation
+# No wait
 defaults write com.apple.dock autohide-time-modifier -int 0
-# Slight animation
+# Slight wait
 # defaults write com.apple.dock autohide-time-modifier -float 0.15
+# Remove animation during show
+defaults write com.apple.dock autohide-delay -float 0
 
 killall Dock
+
+# Keyboard key repeat time
+# Time to initial repeat
+defaults write -g InitialKeyRepeat -int 25
+# Subsequent repeat times
+defaults write -g KeyRepeat -int 1
+
+# Trackpad tracking speed
+defaults write -g com.apple.mouse.scaling -float 2.0
 
