f1dbe7bf16fb999c5b12f9ec121618eee411209b
Jose Ibarra <jose.ibarra@bonus.ly>
2025-02-13 09:08:25 -0700

    Add alacritty config

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -4,9 +4,19 @@
 now=$(date)
 mkdir ~/.dotfiles/backups/"${now}"
 
+# Git config
 mv ~/.gitconfig ~/.dotfiles/backups/"${now}"
 ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
+
+# SSH config
 mv ~/.ssh/config ~/.dotfiles/backups/"${now}"
 ln -s ~/.dotfiles/ssh/config ~/.ssh/config
+
+# ZSH config
 mv ~/.zshrc ~/.dotfiles/backups/"${now}"
 ln -s ~/.dotfiles/shell/.zshrc ~/.zshrc
+
+# Alacritty config
+mv ~/.config/alacritty.toml ~/.dotfiles/backups/"${now}"
+mkdir ~/.config
+ln -F -s ~/.dotfiles/config/alacritty/alacritty.toml ~/.config/alacritty.toml
